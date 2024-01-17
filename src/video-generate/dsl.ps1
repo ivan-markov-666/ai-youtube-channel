682c13651c7315b0016938819a44acd731886e8a
Ivan Markov <IMarkov@objectsystems.com>
2024-01-17 17:55:57 +0200

    everything is done for that milestone

--- src/video-generate/dsl.ps1
+++ src/video-generate/dsl.ps1
@@ -1,251 +1,273 @@
 # Задайте пътя до ffprobe.exe
 $ffprobePath = "C:\Program Files\FFmpeg\ffmpeg-2023-07-02-git-50f34172e0-full_build\bin\ffprobe.exe"
 
-# Функция за извличане на списък с видео файлове (mp4) от дадена директория.
+# Function to retrieve a list of MP4 video files from a given directory.
 function Get-Mp4Files {
     param (
-        [string]$directoryPath
+        [string]$directoryPath  # Parameter for specifying the directory path
     )
+    # Retrieves all .mp4 files within the specified directory and its subdirectories
     $mp4Files = Get-ChildItem -Path $directoryPath -Filter *.mp4 -Recurse -File | ForEach-Object {
-        $_.FullName
+        $_.FullName  # Extracts the full file path of each .mp4 file
     }
-    return $mp4Files
+    return $mp4Files  # Returns the list of MP4 file paths
 }
 
-# Функция за извличане на продължителността на видео файл
+# Function to extract the duration of a video file.
 function Get-VideoDuration {
     param (
-        [string]$videoPath
+        [string]$videoPath  # Parameter for specifying the path to the video file
     )
-    # Изпълнение на ffprobe за получаване на метаданни за видеото
+    # Executes ffprobe to obtain metadata about the video. 
+    # '-v error' sets the logging level to show only errors.
+    # '-show_entries format=duration' requests only the duration data.
+    # '-of default=noprint_wrappers=1:nokey=1' formats the output to be plain text without additional information.
     $output = & $ffprobePath -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$videoPath"
-    # Преобразуване на изхода в число
+
+    # Converts the output to a number and rounds it to two decimal places.
     $duration = [System.Math]::Round($output, 2)
-    return $duration
+
+    return $duration  # Returns the duration of the video
 }
 
-# Функция за извличане на продължителността на аудио файл
+# Function to extract the duration of an audio file.
 function Get-AudioDuration {
     param (
-        [string]$audioPath
+        [string]$audioPath  # Parameter for specifying the path to the audio file
     )
-    # Изпълнение на ffprobe за получаване на метаданни за аудиото
+    # Executes ffprobe to obtain metadata about the audio. 
+    # '-v error' sets the logging level to show only errors.
+    # '-show_entries format=duration' requests only the duration data.
+    # '-of default=noprint_wrappers=1:nokey=1' formats the output to be plain text without additional information.
     $output = & $ffprobePath -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$audioPath"
-    # Преобразуване на изхода в число
+
+    # Converts the output to a number and rounds it to two decimal places.
     $duration = [System.Math]::Round($output, 2)
-    return $duration
+
+    return $duration  # Returns the duration of the audio file
 }
 
-# Функция за избирането на произволен видео файлове от ArrayList с видео файлове. Този видео файл се премахва от списъка, за да не бъде избран отново.
+# Function to randomly select a video file from an ArrayList of video files.
 function Get-RandomVideo {
     param (
-        [System.Collections.ArrayList]$videos
+        [System.Collections.ArrayList]$videos  # Parameter for specifying the ArrayList of video files
     )
-    # Избиране на произволен индекс от списъка с видео файлове
+    # Randomly choose an index from the list of video files
     $randomIndex = Get-Random -Minimum 0 -Maximum $videos.Count
-    # Връщане на произволния видео файл
+
+    # Retrieve the selected video file using the random index
     $selectedVideo = $videos[$randomIndex]
-    # Премахване на избрания видео файл от списъка, за да не бъде избран отново
+
+    # Remove the selected video file from the list to avoid reselection
     $videos.RemoveAt($randomIndex)
-    return $selectedVideo
+
+    return $selectedVideo  # Return the randomly selected video file
 }
 
-# Функция за избиране на произволен видео файлове от масив с видео файлове. Този видео файл се премахва от списъка, за да не бъде избран отново.
-# Проверява се дали продължителността на избрания видео файл е по-голяма или равна на продължителността на аудио файла. Ако е по-малка, то се избира нов произволен видео файл. Това се повтаря докато не се намери видео файл с продължителност по-голяма или равна на продължителността на аудио файла.
+# Function to select random video files from an array of video files. This video file is removed from the list so that it will not be selected again.
+# Checks if the duration of the selected video file is greater than or equal to the duration of the audio file. If it is smaller, a new random video file is selected. This is repeated until a video file with a duration greater than or equal to the duration of the audio file is found.
 function Get-VideoSequenceForAudio {
     param (
-        [System.Collections.ArrayList]$videos,
-        [string]$audioPath
+        [System.Collections.ArrayList]$videos,  # ArrayList of video files
+        [string]$audioPath                      # Path to the audio file
     )
-    # Получаване на продължителността на аудио файла
+    # Get the duration of the audio file
     $totalAudioDuration = Get-AudioDuration -audioPath $audioPath
+    # Initialize an ArrayList to store the selected videos
     $selectedVideos = New-Object System.Collections.ArrayList
+    # Initialize a variable to track the current total duration of selected videos
     $currentDuration = 0
+
+    # Loop until the total duration of selected videos meets or exceeds the audio file's duration
     while ($videos.Count -gt 0 -and $currentDuration -lt $totalAudioDuration) {
-        # Избиране на произволен видео файл
+        # Select a random video file
         $randomVideo = Get-RandomVideo -videos $videos
-        # Получаване на продължителността на избрания видео файл
+        # Get the duration of the selected video file
         $videoDuration = Get-VideoDuration -videoPath $randomVideo
-        # Добавяне на видео файла в крайния списък
+        # Add the selected video to the final list
         [void]$selectedVideos.Add($randomVideo)
-        # Обновяване на текущата продължителност
+        # Update the current total duration
         $currentDuration += $videoDuration
-        # Проверка дали текущата продължителност е по-голяма или равна на продължителността на аудиото
+        # Break the loop if the current duration meets or exceeds the audio duration
         if ($currentDuration -ge $totalAudioDuration) {
             break
         }
     }
-    # Връщане на избраните видео клипове и общата продължителност
+    # Return the list of selected video clips and their total duration
     return $selectedVideos, $currentDuration
 }
 
-# Функция за анализиране на видео файл с ffprobe и записване на резултатите в текстов файл (по подразбиране в същата директория, където се намира видео файлът).
-function Get-VideoFileInformation {
+# Function to merge multiple video files into a single video file.
+function Merge-Videos {
     param (
-        [string]$videoPath,
-        [string]$outputFilePath
+        $videos,               # Array of video file paths to be merged
+        [string]$outputVideoPath  # Path for the output merged video file
     )
-    # Проверка дали файлът за запис съществува
-    if (-not (Test-Path $outputFilePath)) {
-        # Създаване на файл за запис, ако не съществува
-        New-Item -Path $outputFilePath -ItemType "file" -Force
-    }
-    # Изпълнение на ffprobe и записване на резултатите
-    $ffprobeOutput = & $ffprobePath -v error -show_entries stream=width, height, pix_fmt -of default=noprint_wrappers=1:nokey=1 "$videoPath"
-    # Записване на информацията в текстовия файл
-    Add-Content -Path $outputFilePath -Value "Video File: $videoPath"
-    Add-Content -Path $outputFilePath -Value $ffprobeOutput
-    Add-Content -Path $outputFilePath -Value "----------------------"
-}
-
-# Функция за съединяване на видео файлове от масив
-function Merge-Videos($videos, $outputVideoPath) {
+    # Initialize the ffmpeg command
     $ffmpegCommand = "ffmpeg"
-    # Добавяне на видео файловете към командата
+    # Add each video file to the ffmpeg command
     for ($i = 0; $i -lt $videos.Count; $i++) {
         $ffmpegCommand += " -i `"$($videos[$i])`""
     }
 
-    # Създаване на филтъра за мащабиране и съединяване
+    # Create the filter chain for scaling and concatenating
     $filterComplex = ""
     for ($i = 0; $i -lt $videos.Count; $i++) {
-        # Мащабиране и установяване на Display Aspect Ratio (DAR) за всеки видео поток
+        # Scale each video stream to a specific resolution and set the Display Aspect Ratio (DAR)
         $filterComplex += "[${i}:v]scale=1920x1080,setdar=16/9[v$i];"
     }
-    # Конкатениране на обработените видео потоци
+    # Concatenate the processed video streams
     for ($i = 0; $i -lt $videos.Count; $i++) {
         $filterComplex += "[v$i]"
     }
     $filterComplex += "concat=n=$($videos.Count):v=1:a=0[v]"
 
-    # Добавяне на филтъра към командата
+    # Add the filter chain to the ffmpeg command
     $ffmpegCommand += " -filter_complex `"$filterComplex`" -map `[v]` `"$outputVideoPath`""
 
-    # Изпълнение на командата
+    # Execute the ffmpeg command
     Invoke-Expression $ffmpegCommand
 }
 
-# Функция за добавяне на аудио към видео файл.
+# Function to add an audio track to a video file.
 function Add-AudioToVideo {
     param (
-        [string]$videoPath,
-        [string]$audioPath,
-        [string]$outputVideoPath
+        [string]$videoPath,         # Path to the video file
+        [string]$audioPath,         # Path to the audio file
+        [string]$outputVideoPath    # Path for the output video file with the added audio
     )
-    # Получаване на продължителността на видео и аудио файла
+    # Get the durations of both the video and audio files
     $videoDuration = Get-VideoDuration -videoPath $videoPath
     $audioDuration = Get-AudioDuration -audioPath $audioPath
 
-    # Създаване на ffmpeg командата
+    # Initialize the ffmpeg command to combine video and audio
     $ffmpegCommand = "ffmpeg -i `"$videoPath`" -i `"$audioPath`""
 
-    # Проверка дали видеото е по-дълго от аудиото
+    # Check if the video is longer than the audio
     if ($videoDuration -gt $audioDuration) {
-        # Ако видеото е по-дълго, то се срязва до дължината на аудиото
+        # If the video is longer, trim it to match the length of the audio
         $ffmpegCommand += " -t $audioDuration"
     }
 
-    # Добавяне на аудиото към видеото и съхранение на резултата в изходния файл
+    # Add the audio to the video and specify the output file format
+    # '-c:v copy' copies the video stream as is
+    # '-c:a aac' encodes the audio stream to AAC
+    # '-strict experimental' allows using experimental features, necessary for some audio codecs
     $ffmpegCommand += " -c:v copy -c:a aac -strict experimental `"$outputVideoPath`""
 
-    # Изпълнение на командата
+    # Execute the ffmpeg command
     Invoke-Expression $ffmpegCommand
 }
 
-# Функция за извличане на броя на файловете в дадена директория
+# Function to get the count of directories within a specified directory.
 function Get-DirectoryCount {
     param (
-        [string]$directoryPath
+        [string]$directoryPath  # Parameter for specifying the directory path
     )
 
     try {
-        # Вземете всички обекти в директорията и филтрирайте само директориите
+        # Retrieve all items in the directory and filter only the directories
         $directories = Get-ChildItem -Path $directoryPath -Directory
-        # Върнете броя на директориите
+
+        # Return the count of directories
         return $directories.Count
     }
     catch {
-        Write-Host "Грешка: $_"
+        # In case of an error, display an error message and return null
+        Write-Host "Error: $_"
         return $null
     }
 }
 
-# Функция за създаване на директория
+# Function to create a new directory.
 function New-Directory {
     param (
-        [string]$directoryPath
+        [string]$directoryPath  # Parameter for specifying the path of the new directory
     )
 
     try {
-        # Проверете дали директорията вече съществува
+        # Check if the directory already exists
         if (-not (Test-Path -Path $directoryPath)) {
-            # Създайте директорията, ако тя не съществува
+            # Create the directory if it does not exist
             New-Item -Path $directoryPath -ItemType Directory
-            Write-Host "Директорията е създадена успешно: $directoryPath"
+            Write-Host "Directory created successfully: $directoryPath"
         }
         else {
-            Write-Host "Директорията вече съществува: $directoryPath"
+            # Inform if the directory already exists
+            Write-Host "Directory already exists: $directoryPath"
         }
     }
     catch {
-        Write-Host "Грешка при създаването на директорията: $_"
+        # Catch any errors during the directory creation process and display an error message
+        Write-Host "Error creating directory: $_"
     }
 }
 
-# Функция за изтриване на файл
+# Function to remove a file.
 function Remove-File {
     param (
-        [string]$filePath
+        [string]$filePath  # Parameter for specifying the path of the file to be removed
     )
 
     try {
+        # Check if the file exists
         if (Test-Path -Path $filePath) {
+            # Remove the file using Remove-Item cmdlet with Force parameter to ensure deletion
             Remove-Item -Path $filePath -Force
-            Write-Host "Файлът е успешно изтрит: $filePath"
+            Write-Host "File successfully deleted: $filePath"
         }
         else {
-            Write-Host "Файлът не съществува: $filePath"
+            # Inform if the file does not exist
+            Write-Host "File does not exist: $filePath"
         }
     }
     catch {
-        Write-Host "Грешка при изтриването на файла: $_"
+        # Catch any errors during the file removal process and display an error message
+        Write-Host "Error deleting file: $_"
     }
 }
 
+# Function to clear all contents of a specified directory.
 function Clear-Directory {
     param (
-        [string]$directoryPath
+        [string]$directoryPath  # Parameter for specifying the path of the directory to be cleared
     )
 
     try {
-        # Проверка дали директорията съществува
+        # Check if the directory exists
         if (Test-Path -Path $directoryPath) {
-            # Изтриване на всички файлове и поддиректории в директорията
+            # Delete all files and subdirectories within the directory
             Get-ChildItem -Path $directoryPath -Recurse | Remove-Item -Force -Recurse
-            Write-Host "Директорията е изчистена: $directoryPath"
+            Write-Host "Directory has been cleared: $directoryPath"
         }
         else {
-            Write-Host "Директорията не съществува: $directoryPath"
+            # Inform if the directory does not exist
+            Write-Host "Directory does not exist: $directoryPath"
         }
     }
     catch {
-        Write-Host "Грешка при изтриването: $_"
+        # Catch any errors during the clearing process and display an error message
+        Write-Host "Error during deletion: $_"
     }
 }
 
-# Функция за четене на .env файл и извличане на стойностите
+# Function to retrieve a specific value from a .env file based on a key.
 function Get-EnvValue {
     param (
-        [string]$key
+        [string]$key  # Parameter for specifying the key whose value is to be retrieved
     )
 
+    # Read the contents of the .env file, silently continue on error
     $lines = Get-Content ".\.env" -ErrorAction SilentlyContinue
     foreach ($line in $lines) {
+        # Check if the current line contains the key
         if ($line -match "$key=(.*)") {
-            # Тук използваме .Trim('') за да премахнем апострофите от началото и края на стойността
+            # If found, extract the value part using a regex match
+            # Use .Trim('') to remove single quotes from the beginning and end of the value
             return $matches[1].Trim("'")
         }
     }
 
+    # Return null if the key is not found in the .env file
     return $null
 }
